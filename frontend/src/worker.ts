18fd35023dee6f606febd26a20858281a2ccea67
Bryan Chen <bryanchen74@gmail.com>
2024-04-30 00:05:40 -0400

    Update worker response

--- frontend/src/worker.ts
+++ frontend/src/worker.ts
@@ -1,6 +1,16 @@
+import type { Tile } from "./model";
+
 self.addEventListener("message", (evt) => {
   console.log("AI Request:", evt.data);
+  const board: number[] = evt.data.map((x: Tile) =>
+    x === "X" ? 1 : x === "O" ? 2 : 0
+  );
+
+  // For now we return the sum of board mod 7
+  const response = board.reduce((a, v) => a + v, 0) % 7;
+
+  // Simulate a delay
   setTimeout(() => {
-    self.postMessage(Math.floor(Math.random() * 7));
+    self.postMessage(response);
   }, 1000);
 });
